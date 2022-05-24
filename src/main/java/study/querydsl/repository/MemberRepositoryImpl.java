e505de6e45cc52ff492e20f7265fca80c2087cfd
pok3mon025 <pok3mon025@gmail.com>
2022-05-24 23:14:01 +0900

    스프링 데이터 페이징 활용2 - CountQuery 최적화

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -11,14 +11,17 @@ import javax.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.support.PageableExecutionUtils;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
@@ -94,7 +97,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
             .fetch();
 
         // 카운트 쿼리 분리
-        long total = queryFactory
+        JPAQuery<Member> countQuery = queryFactory
             .select(member)
             .from(member)
             .leftJoin(member.team, team)
@@ -102,10 +105,10 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 usernameEq(condition.getUsername()),
                 teamNameEq(condition.getTeamName()),
                 ageGoe(condition.getAgeGoe()),
-                ageLoe(condition.getAgeLoe()))
-            .fetchCount();
+                ageLoe(condition.getAgeLoe()));
 
-        return new PageImpl<>(content, pageable, total);
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+        // return new PageImpl<>(content, pageable, total);
     }
 
     private BooleanExpression usernameEq(String username) {
